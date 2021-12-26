e9c9da45ade83ca3558b283a4991be944700b2be
Karan Malpuriya <76143011+isLookDev@users.noreply.github.com>
2021-12-26 09:51:52 +0530

    Base project folder
    First Unity initialization commit

---
+++ Assets/Notes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+
+public class Notes : MonoBehaviour
+{
+    public static event Action<string> toneName = delegate { };
+
+    private void OnMouseDown () {
+        toneName (name);
+    }
+}
